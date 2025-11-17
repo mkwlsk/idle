3bde49a7b2ce62d91a0b0f377bd0f8c6d70d046a
Mariusz Kowalski <mkwlsk@gmail.com>
2025-11-17 20:55:42 +0100

    Uncomment poweroff command

--- idle.sh
+++ idle.sh
@@ -9,5 +9,5 @@ elif [ $MEDIASERVER_SESSIONS -gt 1 ]; then
     echo "Found $MEDIASERVER_SESSIONS media server session(s). Skipping"
 else
     echo "Found no active sessions. Shutting down."
-#    /usr/sbin/shutdown --poweroff
+    /usr/sbin/shutdown --poweroff
 fi
